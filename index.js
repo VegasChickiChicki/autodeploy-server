7bcf9b02b8fb76970c5b4c33a3cf03f7aa81a344
LasVegas <dodaaa@bk.ru>
2020-10-18 02:26:19 +0300

    Тестовые правки

--- index.js
+++ index.js
@@ -16,19 +16,43 @@ app.listen(2020, () => console.log('Example app listening on port 2020'));
 
 app.post('/', (request, response, next) => {
   console.clear();
-  console.log(request.body);
+  //console.log(request.body);
 
   if (request.headers['x-github-event'] === 'push' && request.body.repository.full_name === 'VegasChickiChicki/nuxt-chat'){
-    chdir('../nuxt-chat', () => {
+    chdir('../nuxt-chat', async () => {
       console.log('process.cwd: ', process.cwd());
 
-      console.log('start git pull && npm run build && npm run start');
+      console.log('start pipeline!');
 
-      child_process.exec('git pull && npm run build && kill -15 `lsof -t -i:3000` && npm run start',(err, stdout) =>{
+      await child_process.exec('git pull',(err, stdout) =>{
         console.log('error: ', err);
         console.log('stdout: ', stdout);
       }).on('exit', code => {
-        console.log('git pull && npm run build && kill -15 `lsof -t -i:3000` && npm run start');
+        console.log('git pull');
+        console.log('code: ', code);
+      });
+
+      await child_process.exec('npm run build',(err, stdout) =>{
+        console.log('error: ', err);
+        console.log('stdout: ', stdout);
+      }).on('exit', code => {
+        console.log('npm run build');
+        console.log('code: ', code);
+      });
+
+      await child_process.exec('kill -15 `lsof -t -i:3000`',(err, stdout) =>{
+        console.log('error: ', err);
+        console.log('stdout: ', stdout);
+      }).on('exit', code => {
+        console.log('kill -15 `lsof -t -i:3000`');
+        console.log('code: ', code);
+      });
+
+      await child_process.exec('npm run start',(err, stdout) =>{
+        console.log('error: ', err);
+        console.log('stdout: ', stdout);
+      }).on('exit', code => {
+        console.log('npm run start');
         console.log('code: ', code);
       });
     });

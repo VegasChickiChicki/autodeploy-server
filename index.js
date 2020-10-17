985f60d94239af87226360ed96cbb08ebe00afb2
LasVegas <dodaaa@bk.ru>
2020-10-17 23:58:37 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const BodyParser = require('body-parser');
+
+const app = express();
+
+app.use(BodyParser.urlencoded({
+  extended: true,
+}));
+
+app.use(BodyParser.json());
+
+app.listen(8080, () => console.log('Example app listening on port 8080'));
+
+app.post('/', request => {
+  console.clear();
+  console.log(request.body);
+
+  next();
+});

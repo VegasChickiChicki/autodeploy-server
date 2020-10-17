1b3ee93f6b34e263c26551184ebf6391054b8fb2
LasVegas <dodaaa@bk.ru>
2020-10-18 00:18:02 +0300

    Тестовые правки

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ app.use(BodyParser.json());
 
 app.listen(2020, () => console.log('Example app listening on port 8080'));
 
+const data = {
+  name: 'vegas',
+  value: 256,
+};
+
+app.get('/', (req, res) => res.send(data));
+
 app.post('/', request => {
   console.clear();
   console.log(request.body);

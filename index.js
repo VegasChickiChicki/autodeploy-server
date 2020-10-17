9f49a76b142b1f64ea3295d974e1e3a1f20c2153
LasVegas <dodaaa@bk.ru>
2020-10-18 01:00:12 +0300

    Тестовые правки

--- index.js
+++ index.js
@@ -20,16 +20,25 @@ app.post('/', (request, response, next) => {
     child_process.exec('cd ..').on('exit', code => {
       console.log('code: ', code);
       console.log('cd ..');
+    }).on('uncaughtException', (err, origin) => {
+      console.log('error: ', err);
+      console.log('origin: ', origin);
     });
 
     child_process.exec('cd nuxt-chat').on('exit', code => {
       console.log('code: ', code);
       console.log('cd nuxt-chat');
+    }).on('uncaughtException', (err, origin) => {
+      console.log('error: ', err);
+      console.log('origin: ', origin);
     });
 
     child_process.exec('git pull && npm run build && pm2 reload nuxt-chat').on('exit', code => {
       console.log('code: ', code);
       console.log('git pull && npm run build && pm2 reload nuxt-chat');
+    }).on('uncaughtException', (err, origin) => {
+      console.log('error: ', err);
+      console.log('origin: ', origin);
     });
   }
 
